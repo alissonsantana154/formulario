54ae9e38e64708ac4ee3b8c1c7ac3f85bac8157f
Alisson Santana <alisson.santana@osbr.com.br>
2022-10-28 16:23:52 -0300

    Exibindo o erro

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let B7validator = {
 
         let inputs = form.querySelectorAll("input");
 
+        B7validator.clearErrors();
+
         for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
             let check = B7validator.checkInput(input);
             if (check !== true) {
                 send = false;
-                console.log(check);
+                B7validator.showError(input, check);
             }
 
             if (send) {
@@ -38,6 +40,26 @@ let B7validator = {
         }
 
         return true;
+    },
+    showError: (input, error) => {
+        input.style.borderColor = "#ff0000";
+
+        let errorElement = document.createElement("div");
+        errorElement.classList.add("error");
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibing);
+    },
+    clearErrors: () => {
+        let inputs = form.querySelectorAll("input");
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style = "";
+        }
+
+        let errorElements = document.querySelectorAll(".error");
+        for (let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
     },
 };
 let form = document.querySelector(".b7validator");
